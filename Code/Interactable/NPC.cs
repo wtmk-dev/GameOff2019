2d44e533c58f42d82f1c92717b9b88ac067bec05
Brandon Khan <38743905+wtmk-dev@users.noreply.github.com>
2019-11-09 00:05:36 -0800

    Npc prefab

---
+++ Code/Interactable/NPC.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject model;
+    [SerializeField]
+    private GameObject conversationTrigger;
+
+}
